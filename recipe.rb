fac9769e4325d9945aee7e0c0ffe5c072ce7f1c8
Kazumasa KUMAMOTO <kumamotone@gmail.com>
2015-07-04 11:25:56 +0900

    spec03: Read recipe from file and puts

--- recipe.rb
+++ recipe.rb
@@ -1,6 +1,16 @@
+# spec01
 # レシピは今オムライスしかない
 def print_recipe_data 
   puts 'オムライス'
 end
 
-print_recipe_data
+# spec02
+def read_recipe_from_file_and_puts
+  File.open('recipe-data.txt') do |file|
+    file.each_line do |recipe|
+      puts recipe
+    end
+  end
+end
+
+read_recipe_from_file_and_puts
